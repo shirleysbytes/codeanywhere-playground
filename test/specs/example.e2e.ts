1ae58fdf705f33b6b48e8865f51406a35d465599
nickashirley+github <nickashirley+github@gmail.com>
2021-08-11 00:15:26 -0400

    minor updates to handle iterating through targets

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,19 +1,22 @@
 import LoginPage from  '../pageobjects/login.page';
 import SecurePage from '../pageobjects/secure.page';
 describe('Scrape', () => {
-    it('should load Target', async () => {
+    before( async () => {
         await browser.setWindowSize(1920, 1080)
-        await browser.url("https://www.target.com/p/playstation-5-console/-/A-81114595#lnk=sametabhttps://www");
-        await browser.saveScreenshot("./screenshots/PS5.png")
-        
-        
-        // await LoginPage.open();
+    })
 
-        // await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        // await expect(SecurePage.flashAlert).toBeExisting();
-        // await expect(SecurePage.flashAlert).toHaveTextContaining(
-        //     'You logged into a secure area!');
-    });
+    const targets = [
+        {store: "Target", url: "https://www.target.com/p/playstation-5-console/-/A-81114595#lnk=sametab"}, 
+        {store: "Newegg", url: "https://www.newegg.com/p/N82E16868110300"},
+        {store: "BestBuy", url: "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"}
+    ]
+
+    targets.forEach((target) => {
+        it(`Should load ${target.store}`, async () => {
+            await browser.url(target.url)
+            await browser.saveScreenshot(`./screenshots/${target.store}.png`)
+        })
+    })
 });
 
 
