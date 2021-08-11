6c2b16bd74cc878e82f6a123f30187cf148a33e3
nickashirley+github <nickashirley+github@gmail.com>
2021-08-10 21:18:55 -0400

    initial commit

---
+++ test/specs/example.e2e.ts
@@ -0,0 +1,18 @@
+import LoginPage from  '../pageobjects/login.page';
+import SecurePage from '../pageobjects/secure.page';
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await browser.setWindowSize(1920, 1080)
+        await browser.url("https://www.target.com/p/playstation-5-console/-/A-81114595#lnk=sametabhttps://www");
+        await browser.saveScreenshot("./PS5.png")
+        
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
+
