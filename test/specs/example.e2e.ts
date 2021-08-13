fd21b023b5959c2914c5f623fb2320d91a71c976
nickashirley+github <nickashirley+github@gmail.com>
2021-08-13 00:09:14 -0400

    removed schedule for now until later updates

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -6,8 +6,8 @@ describe('Scrape', () => {
     })
 
     const targets = [
-        {store: "Target", url: "https://www.target.com/p/playstation-5-console/-/A-81114595#lnk=sametab"}, 
-        {store: "Newegg", url: "https://www.newegg.com/p/N82E16868110300"},
+        {store: "Target", url: "https://www.target.com/p/playstation-5-console/-/A-81114595#lnk=sametab", element: ""}, 
+        {store: "Newegg", url: "https://www.newegg.com/p/N82E16868110300", element: ""},
         // {store: "BestBuy", url: "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"}
     ]
 
@@ -15,6 +15,7 @@ describe('Scrape', () => {
         it(`Should load ${target.store}`, async () => {
             await browser.url(target.url)
             await browser.saveScreenshot(`./screenshots/${target.store}.png`)
+            await expect($(target.element).getText).toContain("Add to Cart")
         })
     })
 });
